ff9cf2cf2204cd0c2d870fb2b5b53acf08b334d3
ohduran <alvaro.duranb@gmail.com>
2018-08-16 16:31:58 +0200

    added first tests to functional_test

--- superlists/functional_test.py
+++ superlists/functional_test.py
@@ -1,7 +1,39 @@
 #!env/bin/python
+import unittest
 from selenium import webdriver
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+"""
+Functional tests, aka acceptance tests, black-box tests.
+User stories as comments
+"""
 
-assert 'Django' in browser.title
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(1)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        """
+        User goes to its homepage, notices that the page title and header
+        mentions a tod-do list.
+        """
+        # Check out homepage
+        self.browser.get('http://localhost:8000')
+
+        # Notice page title and header
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Test completed')
+
+        # Invited to enter a to-do item straight away
+
+        # Types 'buy peacock feathers' into a text box
+
+        # When hitting enter, the page updates and now
+        # the page lists '1: Buy peacock feathers' as an item.
+
+if __name__ == '__main__':
+    unittest.main()
