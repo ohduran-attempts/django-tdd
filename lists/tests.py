3fcb33164b13bcca2e3d4408b44734b12edda4dc
ohduran <alvaro.duranb@gmail.com>
2018-08-16 20:57:46 +0200

    added home page basic website with tests

--- lists/tests.py
+++ lists/tests.py
@@ -3,9 +3,21 @@ This file demonstrates writing tests using the unittest module.
 They will pass when you run 'manage.py test'
 """
 from django.test import TestCase
+from django.http import HttpRequest
+from django.core.urlresolvers import resolve
 
+from lists.views import home_page
 # Create your tests here.
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """Test that 1 + 1 always equals 2."""
-        self.assertEqual(1+1, 2)
+class HomePageTest(TestCase):
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+
+        self.assertTrue(response.content.startswith(b'<html>'))
+        self.assertIn(b'<title>To-Do lists</title>', response.content)
+        self.assertTrue(response.content.endswith(b'</html>'))
+
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
